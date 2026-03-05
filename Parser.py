f49089040121824443406a63e6791c9396d9633e
Daniel Guo <onyxeagle274@gmail.com>
2026-03-05 16:24:25 -0500

    Update Parser.py
    added type effectiveness chart and calculator

--- Parser.py
+++ Parser.py
@@ -5,6 +5,165 @@ import json, os
 base_url = "https://pokeapi.co/api/v2"
 CACHE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache")
 
+type_effectiveness = {
+    "normal":{
+        "rock": 0.5,
+        "ghost": 0,
+        "steel": 0.5,
+    },
+    "fire":{
+        "fire": 0.5,
+        "water": 0.5,
+        "grass": 2,
+        "ice": 2,
+        "bug": 2,
+        "rock": 0.5,
+        "dragon": 0.5,
+        "steel": 2,
+    },
+    "water":{
+        "fire": 2,
+        "water": 0.5,
+        "grass": 0.5,
+        "ground": 2,
+        "rock": 2,
+        "dragon": 0.5,
+    },
+    "grass":{
+        "fire": 0.5,
+        "water": 2,
+        "grass": 0.5,
+        "poison": 0.5,
+        "ground": 2,
+        "flying": 0.5,
+        "bug": 0.5,
+        "rock": 2,
+        "dragon": 0.5,
+        "steel": 0.5,
+    },
+    "electric":{
+        "water": 2,
+        "grass": 0.5,
+        "electric": 0.5,
+        "ground": 0,
+        "flying": 2,
+        "dragon": 0.5,
+    },
+    "ice":{
+        "fire": 0.5,
+        "water": 0.5,
+        "grass": 2,
+        "ice": 0.5,
+        "ground": 2,
+        "flying": 2,
+        "dragon": 2,
+        "steel": 0.5,
+    },
+    "fighting":{
+        "normal": 2,
+        "ice": 2,
+        "poison": 0.5,
+        "flying": 0.5,
+        "psychic": 0.5,
+        "bug": 0.5,
+        "rock": 2,
+        "ghost": 0,
+        "dark": 2,
+        "steel": 2,
+        "fairy": 0.5,
+    },
+    "poison":{
+        "grass": 2,
+        "poison": 0.5,
+        "ground": 0.5,
+        "rock": 0.5,
+        "ghost": 0.5,
+        "steel": 0,
+        "fairy": 2,
+    },
+    "ground":{
+        "fire": 2,
+        "grass": 0.5,
+        "electric": 2,
+        "poison": 2,
+        "flying": 0,
+        "bug": 0.5,
+        "rock": 2,
+        "steel": 2,
+    },
+    "flying":{
+        "grass": 2,
+        "electric": 0.5,
+        "fighting": 2,
+        "bug": 2,
+        "rock": 0.5,
+        "steel": 0.5,
+    },
+    "psychic":{
+        "fighting": 2,
+        "poison": 2,
+        "psychic": 0.5,
+        "dark": 0,
+        "steel": 0.5,
+    },
+    "bug":{
+        "fire": 0.5,
+        "grass": 2,
+        "fighting": 0.5,
+        "poison": 0.5,
+        "flying": 0.5,
+        "psychic": 2,
+        "ghost": 0.5,
+        "dark": 2,
+        "steel": 0.5,
+        "fairy": 0.5,
+    },
+    "rock":{
+        "fire": 2,
+        "ice": 2,
+        "fighting": 0.5,
+        "ground": 0.5,
+        "flying": 2,
+        "bug": 2,
+        "steel": 0.5,
+    },
+    "ghost":{
+        "normal": 0,
+        "psychic": 2,
+        "ghost": 2,
+        "dark": 0.5,
+    },
+    "dragon":{
+        "dragon": 2,
+        "steel": 0.5,
+        "fairy": 0,
+    },
+    "dark":{
+        "fighting": 0.5,
+        "psychic": 2,
+        "ghost": 2,
+        "dark": 0.5,
+        "fairy": 0.5,
+    },
+    "steel":{
+        "fire": 0.5,
+        "water": 0.5,
+        "electric": 0.5,
+        "ice": 2,
+        "rock": 2,
+        "steel": 0.5,
+        "fairy": 2,
+    },
+    "fairy":{
+        "fire": 0.5,
+        "fighting": 2,
+        "poison": 0.5,
+        "dragon": 2,
+        "dark": 2,
+        "steel": 0.5,
+    }
+}
+
 #fetches pokemon by id
 def fetch_pokemon(pokemon_id):
     if int(pokemon_id) < 1 or int(pokemon_id) > 1025:
@@ -192,6 +351,16 @@ def calculate_stats(pokemon_id):
     replace_data(path, pokemon_data)
     return max_stats
 
+def calculate_type_effectiveness(atk_type, def_type1, def_type2):
+    type1_effectiveness = 1
+    type2_effectiveness = 1
+    if def_type1 in type_effectiveness[atk_type]:
+        type1_effectiveness = type_effectiveness[atk_type][def_type1]
+    if def_type2 in type_effectiveness[atk_type]:
+        type2_effectiveness = type_effectiveness[atk_type][def_type2]
+
+    return 1 * type1_effectiveness * type2_effectiveness
+
 def calculate_damage(pokemon1_id, pokemon2_id):
     #general data
     pokemon1_data = fetch_pokemon(pokemon1_id)
@@ -229,6 +398,8 @@ def calculate_damage(pokemon1_id, pokemon2_id):
         is_triple_axel = True
     if move["move"]["name"] == "triple-kick":
         is_triple_kick = True
+
+    
 
     if is_triple_axel:
         return
