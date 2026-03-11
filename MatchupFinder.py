97344fcee537b3b0ff8035f53975c447a4752229
Daniel Guo <onyxeagle274@gmail.com>
2026-03-11 00:21:01 -0400

    375 matches

--- MatchupFinder.py
+++ MatchupFinder.py
@@ -6,12 +6,9 @@ CACHE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache")
 OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output")
 
 if __name__ == "__main__":
-    pokemon1_name = "charizard"
-    pokemon2_name = "blastoise"
+    pokemon1_name = "tauros"
+    pokemon2_name = "rhydon"
 
-    path = os.path.join(OUTPUT_DIR, "matchups.json")
-    with open(path) as f:
-        data = json.load(f)
     
     url = f"{base_url}/pokemon/{pokemon1_name}"
     response = re.get(url).json()
@@ -22,6 +19,11 @@ if __name__ == "__main__":
     response = re.get(url).json()
     pokemon2_id = str(response["id"])
 
+    path = os.path.join(OUTPUT_DIR, "matchups_" + str(pokemon1_id) + ".json")
+    data = {}
+    with open(path) as f:
+        data = json.load(f)
+    
     if pokemon1_id + "_vs_" + pokemon2_id in data.keys():
         print(data[pokemon1_id + "_vs_" + pokemon2_id])
     elif pokemon2_id + "_vs_" + pokemon1_id in data.keys():
